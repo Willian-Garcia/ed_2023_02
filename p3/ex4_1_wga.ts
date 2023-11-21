ea947377505e25266a41dcad89c8bd46118893a4
Willian Garcia <williangarcia710@hotmail.com>
2023-11-20 21:14:44 -0300

    P3 finalizada

--- p3/ex4_1_wga.ts
+++ p3/ex4_1_wga.ts
@@ -1,84 +1,80 @@
-//P.4.1. Desenvolva um programa que leia os dados de um arquivo de texto e que armazene cada uma das palavras em uma lista encadeada (cada nó da lista contém uma palavra). Depois de carregar os dados na lista, o programa solicita ao usuário a digitação de uma palavra e então faz a busca sequencial e informa a posição da palavra na lista. Caso a palavra apareça mais de uma vez, o programa deve informar isso na saída.
-//ARMAZENA DE FORMA GLOBAL O QUE É LIDO DO TXT
-
-
-import { MyNode, List } from "./list";
-import * as fs from "fs";
+import { List, MyNode } from "./list";
+import * as fs from 'fs';
 import * as readline from "readline";
 
-let conteudoDoArquivo: string;
+function lerArquivoEArmazenarEmLista(texto: string): List<string> {
+    const listaPalavras = new List<string>();
 
-// CAMINHO DO ARQUIVO A SER LIDO
-const caminhoDoTxt = './p3/texto.txt';
+    try {
+        const conteudoArquivo = fs.readFileSync(texto, 'utf-8');
+        const palavras = conteudoArquivo.split(/\s+/).filter(palavra => palavra.trim() !== '');
 
-// FUNÇÃO DE LEITURA
-function lerArquivoTxt(caminhoDoTxt: string, callback: (error: Error | null, data: string | null) => void) {
-  fs.readFile(caminhoDoTxt, 'utf-8', (err, data) => {
-    if (err) {
-      console.error('Erro ao ler o arquivo:', err);
-      callback(err, null);
-    } else {
-      console.log('Conteúdo do arquivo lido com sucesso:', data);
-      conteudoDoArquivo = data;
-      callback(null, data);
+        for (const palavra of palavras.reverse()) {
+            const node = new MyNode<string>(palavra);
+            listaPalavras.push(node);
+        }
+
+        return listaPalavras;
+    } catch (error) {
+        console.error('Erro ao ler o arquivo:', error);
+        throw error;
     }
-  });
 }
 
-// CHAMADA DA FUNÇÃO PARA LEITURA
-lerArquivoTxt(caminhoDoTxt, (error, conteudoDoArquivo) => {
-  if (conteudoDoArquivo !== null) {
-    const wordList = new List<string>();
+function encontrarPalavraNaLista(lista: List<string>, palavra: string): { posicoes: number[], quantidade: number } {
+    const posicoes: number[] = [];
+    let currentNode = lista.start;
+    let index = 0; // Começamos a contar do número 1
+    let quantidade = 0;
 
-    // Divide o conteúdo em palavras e adiciona cada palavra à lista
-    const separarPalavras = conteudoDoArquivo.split(' ');
-    for (const palavra of separarPalavras) {
-      const node = new MyNode<string>(palavra);
-      wordList.push(node);
+    while (currentNode) {
+        if (currentNode.value === palavra) {
+            posicoes.push(index);
+            quantidade++;
+        }
+
+        currentNode = currentNode.next;
+        index++;
     }
 
-    // Agora você pode usar a função findWordPositions para encontrar uma palavra na lista.
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+    return { posicoes, quantidade };
+}
 
-    rl.question("Digite a palavra que deseja buscar: ", (userInput: string) => {
-      const result = findWordPositions(wordList, userInput);
-      if (result.positions.length > 0) {
-        console.log(`A palavra "${userInput}" aparece nas posições:`);
-        for (let i = 0; i < result.positions.length; i++) {
-          console.log(`- Posição ${result.positions[i]}`);
-        }
-        console.log(`A palavra "${userInput}" aparece ${result.count} vezes.`);
-      } else {
-        console.log(`A palavra "${userInput}" não foi encontrada no arquivo.`);
-      }
-      rl.close();
-    });
-  }
+// Exemplo de uso
+const caminhoDoArquivo = './p3/texto.txt';
+const listaDePalavras = lerArquivoEArmazenarEmLista(caminhoDoArquivo);
+
+// Exibe todo o texto do arquivo
+console.log('Conteúdo do arquivo:');
+const conteudoArquivo = fs.readFileSync(caminhoDoArquivo, 'utf-8');
+console.log(conteudoArquivo);
+
+// Imprime a lista de palavras
+/*console.log('Lista de Palavras:');
+let currentNode = listaDePalavras.start;
+while (currentNode) {
+    console.log(currentNode.value);
+    currentNode = currentNode.next;
+}
+*/
+
+// Solicita ao usuário a palavra a ser buscada
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 });
 
-// Função para buscar a posição de uma palavra na lista
-function findWordPositions(wordList: List<string>, targetWord: string): { positions: number[], count: number } {
-  const positions: number[] = [];
-  let currentNode: MyNode<string> | undefined = wordList.start;
-  let index = 1; // Começamos a contar do número 1
-  let count = 0;
-
-  while (currentNode) {
-    if (currentNode.value === targetWord) {
-      positions.push(index);
-      count++;
+rl.question('Digite a palavra que deseja buscar: ', (palavraABuscar) => {
+    const result = encontrarPalavraNaLista(listaDePalavras, palavraABuscar);
+    if (result.quantidade > 0) {
+        console.log(`A palavra '${palavraABuscar}' aparece nas posições:`);
+        for (let i = 0; i < result.posicoes.length; i++) {
+            console.log(`- Posição ${result.posicoes[i]}`);
+        }
+        console.log(`A palavra '${palavraABuscar}' aparece ${result.quantidade} vezes.`);
+    } else {
+        console.log(`A palavra '${palavraABuscar}' não foi encontrada na lista.`);
     }
 
-    currentNode = currentNode.next;
-    index++;
-  }
-
-  return { positions, count };
-}
-
-
-
-
+    rl.close();
+});
